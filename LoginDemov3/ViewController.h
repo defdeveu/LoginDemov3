15fa0631cafd54bd1bb05275241f33b5d3957c80
huobb0 <zsombor.kovacs@gmail.com>
2018-02-16 16:47:25 +0100

    Working 1.0 version

--- LoginDemov3/ViewController.h
+++ LoginDemov3/ViewController.h
@@ -8,8 +8,9 @@
 
 #import <UIKit/UIKit.h>
 
-@interface ViewController : UIViewController
+@interface ViewController : UIViewController <UITextFieldDelegate>
 
+- (IBAction)Login2ButtonPressed:(id)sender;
 
 @end
 
--- LoginDemov3/ViewController.m
+++ LoginDemov3/ViewController.m
@@ -8,22 +8,72 @@
 
 #import "ViewController.h"
 
-@interface ViewController ()
-
+@interface ViewController () <UITextFieldDelegate>
+@property (weak, nonatomic) IBOutlet UITextField *UsernameField;
+@property (weak, nonatomic) IBOutlet UIButton *LoginButton;
+@property (weak, nonatomic) IBOutlet UITextField *PasswordField;
 @end
 
 @implementation ViewController
 
+- (NSDictionary*)passwordStore {
+    NSString* filename = [[NSBundle mainBundle] pathForResource:@"Accounts"
+                                                         ofType:@"plist"
+                                                    inDirectory:nil];
+    return [[NSDictionary alloc] initWithContentsOfFile:filename];
+}
+
+- (BOOL)checkLogin{
+    NSString* userKeyPath = [NSString stringWithFormat:@"Username"];
+    NSString* passKeyPath = [NSString stringWithFormat:@"Password"];
+    NSString* expectedUsername = [self.passwordStore valueForKeyPath:userKeyPath];
+    NSString* expectedPassword = [self.passwordStore valueForKeyPath:passKeyPath];
+    //NSLog(expectedUsername);
+    //NSLog(expectedPassword);
+    return ([_UsernameField.text isEqual: expectedUsername] && [_PasswordField.text isEqual: expectedPassword]);
+    }
+
+- (IBAction)LoginButtonPressed:(id)sender {
+    if ([self checkLogin]) {
+        [[[UIAlertView alloc] initWithTitle:@"Login result" message:@"Logged in successfully!"
+                                   delegate:nil cancelButtonTitle:@"OK" otherButtonTitles:nil] show];
+    }else{
+        [[[UIAlertView alloc] initWithTitle:@"Login result" message:@"Failed."
+                                   delegate:nil cancelButtonTitle:@"Sorry" otherButtonTitles:nil] show];
+    }
+}
+
+- (BOOL)checkLoginHardcoded{
+    return ([_UsernameField.text isEqual: @"frodo"] && [_PasswordField.text isEqual: @"lives"]);
+}
+
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    _UsernameField.delegate = self;
+    _PasswordField.delegate = self;
 }
 
+- (BOOL)textFieldShouldReturn:(UITextField *)textField {
+    [textField resignFirstResponder];
+    return YES;
+}
+
+- (void)textFieldDidEndEditing:(UITextField *)textField{
+}
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
 
-
+- (IBAction)Login2ButtonPressed:(id)sender {
+    if ([self checkLoginHardcoded]) {
+        [[[UIAlertView alloc] initWithTitle:@"Login result" message:@"Logged in successfully!"
+                                   delegate:nil cancelButtonTitle:@"OK" otherButtonTitles:nil] show];
+    }else{
+        [[[UIAlertView alloc] initWithTitle:@"Login result" message:@"Failed."
+                                   delegate:nil cancelButtonTitle:@"Sorry" otherButtonTitles:nil] show];
+    }
+}
 @end
